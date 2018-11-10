b5958bc1501b00d233484872f80ea8f028540d34
birametgod <birametgod95@gmail.com>
2018-11-10 16:40:06 +0100

    creation des models

--- listing/db/schema.rb
+++ listing/db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_171342) do
+ActiveRecord::Schema.define(version: 2018_11_10_153852) do
 
   create_table "announcements", force: :cascade do |t|
     t.string "titre"
     t.text "description"
     t.integer "prix"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "nom"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "contenu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -28,6 +40,8 @@ ActiveRecord::Schema.define(version: 2018_11_09_171342) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
