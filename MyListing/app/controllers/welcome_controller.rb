554b4ba44bf8b9f19289c994d42246ad47891a55
birametgod <birametgod95@gmail.com>
2018-12-13 17:18:40 +0100

    creer un champ de recherche

--- MyListing/app/controllers/welcome_controller.rb
+++ MyListing/app/controllers/welcome_controller.rb
@@ -1,5 +1,3 @@
 class WelcomeController < ApplicationController
-  def new
 
-  end
 end
--- MyListing/app/models/listing.rb
+++ MyListing/app/models/listing.rb
@@ -3,4 +3,12 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_one :picture, as: :imageable
   accepts_nested_attributes_for :picture #pour permettre l'upload de fichier
+
+  def self.search(keywords)
+    if keywords.blank?
+      self.all.order('created_at DESC')
+    else
+      self.where('titre LIKE ?', "%#{keywords}%") #ce qui est contenu dans la variable keywords peut etre se trouver n'importe ou dans le titre
+    end
+  end
 end
