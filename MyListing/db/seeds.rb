0893570a96223b799dd7b6fc8de371509d7c8db7
birame <birametgod95@gmail.com>
2019-04-14 16:06:27 +0200

    seul les utilisateurs connectés peuvent acceder à la partie admin

--- MyListing/db/seeds.rb
+++ MyListing/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(nom:'Voitures')
+Category.create(nom: 'Rappeurs')
+Category.create(nom: 'Vêtements')
